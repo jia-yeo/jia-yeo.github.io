1a19eb44bc9df64eaa0147f432a5320ce0e3d822
jia-yeo <secondattempt@naver.com>
2020-08-14 16:10:47 +0900

    dfs

---
+++ pages/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <article id="profile">
+    <p>profile 페이지다</p>
+  </article>
+</template>
+
+<script>
+  import { mapState, mapMutations, mapActions } from 'vuex';
+
+  export default {
+    data: () => ({
+      title: ''
+    }),
+    methods: {}
+  }
+</script>
+
+<style lang="scss">
+#profile {padding: 50px;}
+</style>
